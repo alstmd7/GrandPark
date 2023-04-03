020113eed7b21231d708f8edb010bf2a77e8b3eb
Subin <cc030110@gmail.com>
2023-04-03 14:41:26 +0900

    04.03메인,공지

--- js/menu.js
+++ js/menu.js
@@ -19,26 +19,8 @@ function slideMenu() {
 
 }
 
-
 // 윈도우 로드 시 실행
 $(document).ready(function () {
     // 메인 슬라이드 메뉴
     slideMenu();
-
-    // bxSlider 인스턴스 생성
-    var slider = $('.bxslider').bxSlider({
-        controls: false,    // 이전 다음 버튼 노출 여부
-        moveSlides: 1,     // 슬라이드 이동시 개수
-        pager: false,      // 현재 위치 페이징 표시 여부 설정
-        wrapperClass:'slider_wrap' // 기본 bx-wrapper
-    });
-
-    //이미지 버튼 클릭 시 이전/다음 슬라이드로 이동
-    $('#slider_prev').click(function () {
-        slider.goToPrevSlide();
-    });
-    $('#slider_next').click(function () {
-        slider.goToNextSlide();
-    });
-
 });
