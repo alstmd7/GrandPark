ca7647b46866e017ebcb1d47715fbfbb573fc81a
seongjj <ddz77ddz77@hotmail.com>
2023-04-03 14:46:04 +0900

    4월3일_소개페이지 모바일제작

--- js/intro.js
+++ js/intro.js
@@ -1,18 +1,20 @@
+/* 탭 메뉴 선택시 이미지, 텍스트 교체 */
 $(document).ready(function() {
   // 첫번째 탭 활성화
-  $('.tab_menu li:first').addClass('active');
-  $('.tab_content div:first').addClass('active');
+  $('.intro_img li:first').addClass('active');
+  $('.intro_title div:first').addClass('active');
 
   // 탭 메뉴 클릭 이벤트 처리
-  $('.tab_menu a').click(function(event) {
+  $('.inttab_menu a').click(function(event) {
     event.preventDefault();
 
     // 탭 메뉴 활성화/비활성화 처리
-    $('.tab_menu li').removeClass('active');
+    $('.inttab_menu li').removeClass('active');
     $(this).parent().addClass('active');
 
     // 탭 콘텐츠 활성화/비활성화 처리
-    $('.tab_pane').removeClass('active');
+    $('.intro_img').removeClass('active');
     $($(this).attr('href')).addClass('active');
   });
 });
+
