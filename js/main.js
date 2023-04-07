9ea970169657c63330d480cb8f352a26c345b138
Subin <cc030110@gmail.com>
2023-04-07 17:34:58 +0900

    04.07메인

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ $(document).ready(function () {
         controls: false,    // 이전 다음 버튼 노출 여부
         moveSlides: 1,     // 슬라이드 이동시 개수
         pager: false,      // 현재 위치 페이징 표시 여부 설정
-        wrapperClass:'slider_wrap' // 기본 bx-wrapper
+        wrapperClass:'slider_wrap', // 기본 bx-wrapper
+        adaptiveHeight: true // 높이 자동 조절
     });
 
     //이미지 버튼 클릭 시 이전/다음 슬라이드로 이동
